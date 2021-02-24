72700c231c7aa18ff92afcf3061bf50802cbb627
Odysseus7 <chalitandunu130@gmail.com>
2021-02-24 20:37:55 +0100

    Add state to searchbar

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import './SearchBar.scss';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            keyword: ""
+        }
+    }
+
+    handleSearchSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.keyword);
+    }
+
     render() {
         return (
-            <div className="searchbar">
-                <input type="text" placeholder="Recipe search" />
-            </div>
+            <form className="searchbar" onSubmit={this.handleSearchSubmit}>
+                <input type="text" placeholder="Recipe search" value={this.state.keyword} onChange={(event) => this.setState({keyword: event.target.value})}/>
+            </form>
         )
     }
 }
