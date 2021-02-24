70d5d705e262c257397fdee26073d8e9df8c8215
Odysseus7 <chalitandunu130@gmail.com>
2021-02-24 21:45:26 +0100

    Lay out grid items

--- src/components/Recipes/RecipeList.jsx
+++ src/components/Recipes/RecipeList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import RecipeCard from './RecipeCard';
+import './RecipeList.scss';
 
 const RecipeList = ({recipesArray}) => {
     const recipes = recipesArray.map(recipe => {
