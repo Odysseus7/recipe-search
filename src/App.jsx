799a603429cd5b73052190c456b9f914919b683b
Odysseus7 <chalitandunu130@gmail.com>
2021-02-25 06:49:09 +0100

    Link to forkifyAPI on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,30 @@ import SearchBar from './components/Searchbar/SearchBar';
 import './App.scss';
 import axios from 'axios';
 import RecipeList from './components/Recipes/RecipeList';
+import Keywords from './Keywords';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            recipes: []
+            recipes: null
         }
     }
 
     handleSearchSubmit = async (keyword) => {
         const recipes = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${keyword}`);
         this.setState({recipes: recipes.data.recipes})
-        console.log(this.state.recipes);
+        
     }
 
     render() {
         return (
             <div className="container">
                 <SearchBar onSubmit={this.handleSearchSubmit} />
-                <RecipeList recipesArray={this.state.recipes} />
+                {
+                    this.state.recipes ? <RecipeList recipesArray={this.state.recipes} /> : <Keywords />
+                }
+                
             </div>
         )
     }
---
+++ src/Keywords.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Keywords.scss';
+
+const Keywords = () => {
+    return (
+        <div class="searchqueries">
+            <p>Find the available searchqueries on <a href="https://forkify-api.herokuapp.com/phrases.html" target="_blank">the forkify api</a></p>
+        </div>
+    )
+}
+
+export default Keywords;
